b32c8a0accf8565d1e53eb068a26ed472b32d25f
Matthew Tobin <tobin.matthew94@gmail.com>
2023-02-26 20:01:24 -0800

    added connection to mongodb, created User model

---
+++ Develop/models/User.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require('mongoose');
+
+var validateEmail = (email) => {
+    var regex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+    return regex.test(email)
+};
+
+const userSchema = new Schema(
+    {
+        //string, unique, required, trimmed
+        username: { type: String, unique: true, required: true, trim: true },
+        //string, required, unique, must match valid email address --> Mongoose matching validation
+        email: { 
+            type: String, 
+            unique: true, 
+            required: true,
+            validate: [validateEmail, 'Please input a valid email address'],
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please input a valid email address'],
+        },
+        //array of _id values referencing the Thought model
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        //array of _id values referencing the User model (self-reference)
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+);
+
+//create a virtual called "friendCount" that retrieves the length of the user's 'friends' array field on query
+userSchema
+    .virtual('friendCount')
+    .get(function () {
+        return this.friends.length;
+    });
+
+const User = model('user', userSchema);
+
+module.exports = User;
--- Develop/server.js
+++ Develop/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
 
 const PORT = 3001;
-
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
-app.use('/api/users', require('./routes/api/userRoutes'));
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+    });
+});
